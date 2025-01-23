4e40285572dd3fadd541e0e592e5f5e21b20569c
Czesiek1701 <grzesiekczczaja@gmail.com>
2025-01-23 20:09:03 +0100

    copy old base

---
+++ build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_pecker_vt_FILE D:/Magazyn/Studnia_czasu/dev_cpp/Run-Pecker/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/pecker_vt.exe)
+set(__QT_DEPLOY_TARGET_pecker_vt_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_pecker_vt_RUNTIME_DLLS D:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
